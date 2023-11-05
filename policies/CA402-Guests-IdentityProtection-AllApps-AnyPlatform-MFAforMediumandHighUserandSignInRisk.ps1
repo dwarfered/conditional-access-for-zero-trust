b99ba13e179b8c8b429214d062c6fe34456b6171
Chris Dymond <dwarfered@users.noreply.github.com>
2023-11-05 19:24:57 +0800

    Update CA402-Guests-IdentityProtection-AllApps-AnyPlatform-MFAforMediumandHighUserandSignInRisk.ps1

--- policies/CA402-Guests-IdentityProtection-AllApps-AnyPlatform-MFAforMediumandHighUserandSignInRisk.ps1
+++ policies/CA402-Guests-IdentityProtection-AllApps-AnyPlatform-MFAforMediumandHighUserandSignInRisk.ps1
@@ -12,7 +12,7 @@ $ErrorActionPreference = 'stop'
         Guests perform MFA for medium and high risks.
 
     .NOTES
-        AUTHOR: https://github.com/dwarfered/ConditionalAccessforZeroTrust
+        AUTHOR: https://github.com/dwarfered/conditional-access-for-zero-trust
         UPDATED: 04-11-2023
 #>
 
